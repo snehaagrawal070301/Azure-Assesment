9871e6ccfa7019b6f8089ef6e021735302f8f8f0
Adarsh Kunwar <adarsh.infinite@gmail.com>
2023-09-10 13:07:56 +0530

    service bus

---
+++ Service bus/WebApplication1/Employee.cs
@@ -0,0 +1,16 @@
+﻿using Azure.Data.Tables;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace WebApplication1
+{
+    public class Employee
+    {
+        [Key]
+        [JsonProperty("id")]
+        public string employeeId { get; set; }
+        public string employeeName { get; set; }
+        public string role { get; set; }
+    }
+}
